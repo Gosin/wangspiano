c35fec48d455f83f2443adfec6cb7d294b5e3484
Gosin <canling0@gmail.com>
2024-05-06 16:27:17 -0500

    Start building website using nuxt 3

---
+++ app.vue
@@ -0,0 +1,47 @@
+<script setup>
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    },
+    {
+      rel: 'icon',
+      href: '/favicon-32x32.png'
+    },
+    {
+      rel: 'icon',
+      href: '/favicon-16x16.png'
+    },
+    {
+      rel: 'icon',
+      href: '/apple-touch-icon.png'
+    },
+    {
+      rel: 'icon',
+      href: '/android-chrome-512x512.png'
+    },
+    {
+      rel: 'icon',
+      href: '/android-chrome-192x192.png'
+    }
+  ],
+  title: "Wang's Piano 王的琴"
+})
+</script>
+
+<template>
+  <title>Wang's Piano</title>
+  <div>
+    <h1>Wang's Piano （王的琴）</h1>
+    <h2>Under construction</h2>
+    <h1>🎉 Happy Mother's Day to Eleanor's Mother Mrs. Wang </h1>
+  </div>
+</template>
+
+<style>
+h1, h2 {
+  text-align: center;
+}
+</style>
